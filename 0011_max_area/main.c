76d60424e3bd0957aa6625fdbfe4e2962369f2c2
hmj6639 <hmj6639@126.com>
2018-02-05 20:36:42 +0800

    11

--- 0011_max_area/main.c
+++ 0011_max_area/main.c
@@ -2,24 +2,33 @@
 #include <string.h>
 #include <stdlib.h>
 
+
+
 int maxArea(int* height, int heightSize) {
-	
+
 	int max = 0, left = 0, right = heightSize - 1;
 
 	if(height == NULL || heightSize < 2)
 		return 0;
 
-	for(int i = 0; i < heightSize; i++)
-		if(height[i] < 1)
-			return 0;
-
 	if(heightSize == 2)
 		return height[0] < height[1]? height[0]:height[1];
-	
+
 	while(left < right) {
 
-		int small = (height[left] < height[right] ? height[left] : height[right]);
-		int area = small * (right - left);
+		int small, area;
+
+		if(height[left] == 0) {
+			left++;
+			continue;
+		}
+
+		if(height[right] == 0) {
+			right--;
+			continue;
+		}
+		small = (height[left] < height[right] ? height[left] : height[right]);
+		area = small * (right - left);
 
 		if(max < area)
 			max = area;
@@ -29,12 +38,12 @@ int maxArea(int* height, int heightSize) {
 		else
 			right--;
 	}
-	printf("max %d\n", max);	
+	//printf("max %d\n", max);    
 	return max;
 }
 
 int main(int argc, char *argv[])
 {
-	int a[] = {1, 1};
-	maxArea(a, 2);
+	int a[] = {6,14,2,11,2,7,0,9,12,7};
+	maxArea(a, sizeof(a) / sizeof(a[0]));
 }
