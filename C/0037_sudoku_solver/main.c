003f8f81eb163fe08a227d4e9049a6767cd4a640
hmj6639 <hmj6639@126.com>
2018-02-25 18:12:56 +0800

    37

--- C/0037_sudoku_solver/main.c
+++ C/0037_sudoku_solver/main.c
@@ -11,45 +11,17 @@ void dump(char *board, int boardRowSize, int boardColSize) {
 	printf("\n\n");
 }
 
-void dumpCo(int *co[][9])
-{
-	for(int i = 0; i < 9; i++)
-		for(int j = 0; j < 9; j++) {
-			int *tmp = co[i][j];
-
-			printf("[%d, %d]: ", i, j);
-			for(int k = 1; k < 10; k++) {
-				//printf("%d(%d) ", tmp[k],k);
-
-				if(tmp[k] == 0)
-					printf("%d ", k);
-			}
-
-			printf("\n");
-		}
-}
-
-void opCo(int *co[][9], int boardRowSize, int boardColSize, int flag)
-{
-	for(int i = 0; i < boardRowSize; i++)
-		for(int j = 0; j < boardColSize; j++)
-			if(flag == 0)
-				co[i][j] = (int *)calloc(sizeof(int), 10);
-			else
-				free(co[i][j]);
-}
-
 void mark(int *tmp, char v)
 {
 	if(v != '.')
-		tmp[v - '0'] = 1;
+		*tmp = *tmp | (1 << (v - '0'));
 }
 
 void updateCo(char **board, int boardRowSize, int boardColSize, int row, int col, int *tmp)
 {
 	int r = row - row % 3, c = col - col % 3;
 
-	tmp[0] = 1;
+	*tmp = *tmp | 0x01;;
 	for(int i = 0; i < boardColSize; i++) {
 
 		if(i == col) continue;
@@ -71,17 +43,16 @@ void updateCo(char **board, int boardRowSize, int boardColSize, int row, int col
 	}
 }
 
-void updateCos(char** board, int boardRowSize, int boardColSize, int *co[][9]) 
+void updateCos(char** board, int boardRowSize, int boardColSize, int co[9][9]) 
 {
 	for(int i = 0; i < boardRowSize; i++) {
 
 		for(int j = 0; j < boardColSize; j++) {
 
 			if(board[i][j] == '.') 
-				updateCo(board, boardRowSize, boardColSize, i, j, co[i][j]);
+				updateCo(board, boardRowSize, boardColSize, i, j, &co[i][j]);
 			else
-				for(int k = 0; k < 10; k++)
-					co[i][j][k] = 1;
+				co[i][j] = 0xffffffff;
 		}
 	}
 	//dumpCo(co);
@@ -89,14 +60,13 @@ void updateCos(char** board, int boardRowSize, int boardColSize, int *co[][9])
 
 int tryKatIJ(char** board, int boardRowSize, int boardColSize, int i, int j, int k)
 {
-	int tmp[10];
+	int tmp = 0x01;
 
-	for(int z = 0; z < 10; z++) tmp[z] = 0;
-	updateCo(board,boardRowSize, boardColSize, i, j, tmp);
-	return (tmp[k] == 0) ? 0 : -1;
+	updateCo(board,boardRowSize, boardColSize, i, j, &tmp);
+	return (tmp & (1 << (k))) == 0 ? 0 : -1;
 }
 
-int solveSudoku2(char** board, int boardRowSize, int boardColSize, int i, int j, int *co[][9])
+int solveSudoku2(char** board, int boardRowSize, int boardColSize, int i, int j, int co[9][9])
 {
 	if(j == boardColSize) {
 		if(++i == boardRowSize)
@@ -107,7 +77,7 @@ int solveSudoku2(char** board, int boardRowSize, int boardColSize, int i, int j,
 	if(board[i][j] == '.') {
 		for(int k = 1; k < 10; k++) {
 
-			if((co[i][j][k] == 1) || (tryKatIJ(board, boardRowSize, boardColSize, i, j, k) != 0))
+			if(((co[i][j] & (1 << k)) != 0) || (tryKatIJ(board, boardRowSize, boardColSize, i, j, k) != 0))
 				continue;
 
 			board[i][j] = k + '0';
@@ -123,15 +93,12 @@ int solveSudoku2(char** board, int boardRowSize, int boardColSize, int i, int j,
 
 void solveSudoku(char** board, int boardRowSize, int boardColSize) {
 
-	int* co[9][9];
+	int co[9][9] = {{0,},};
 
 	if(board == NULL) return;
 	
-	opCo(co, boardRowSize, boardColSize, 0);
 	updateCos(board, boardRowSize, boardColSize, co);
-
 	solveSudoku2(board, boardRowSize, boardColSize, 0, 0, co);
-	opCo(co, boardRowSize, boardColSize, 1);
 }
 
 int main()
