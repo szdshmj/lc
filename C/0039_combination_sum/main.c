4f35231e9d99c1e0ea20ce455d7b63190a0e02be
hmj6639 <hmj6639@126.com>
2018-02-26 15:41:48 +0800

    39

--- C/0039_combination_sum/main.c
+++ C/0039_combination_sum/main.c
@@ -2,53 +2,53 @@
 #include "../inc.h"
 #include "../sort.h"
 
-int** combinationSum2(int* candidates, int candidatesSize, int target, int** columnSizes, int* returnSize) {
-
-	int **result = NULL;
-
-	if(candidatesSize == 1) {
-		if(candidates[0] != target) {
-
-			*columnSizes = NULL;	
-			*returnSize = 0;
-			return NULL;
-		}	
-		else {
-			*columnSizes = (int *)malloc(sizeof(int));
-			*columnSizes[0] = 1;
-			result = (int**) malloc(sizeof(int *));
-			result[0] = (int *) malloc(sizeof(int));
-			result[0][0] = target;
-			*returnSize = 1;	
-			return result;
-		}
+int** record(int **result, int** columnSizes, int* returnSize, int *pre, int n)
+{
+	if(*returnSize == 0) {
+		*columnSizes = malloc(sizeof(int));
+		result = (int **)malloc(sizeof(int *));
 	}
 	else {
-		int returnSize_;
-		int *columnSizes_ = NULL;
-		int **sub;
-
-		for(int i = 0; i < candidatesSize; i++) {
-			for(int j = 0; j < INT_MAX; j++) {
-				if(candidates[i] * j < target) {
-
-					returnSize_ = 0;
-					columnSizes_ = NULL;
-					if(candidatesSize - i - 1 > 0)
-						sub = combinationSum2(candidates + i + 1, candidatesSize - i - 1, target - candidates[i] * j, &columnSizes_, &returnSize_);
-
-					if(sub == NULL) continue;
-
-
-				}
-			}
-		}
+		*columnSizes = (int *)realloc(*columnSizes, sizeof(int) * (*returnSize + 1));
+		result = (int **)realloc(result, sizeof(int *) * (*returnSize + 1));
 	}
+	
+	(*columnSizes)[*returnSize] = n;
+	result[*returnSize] = (int *) malloc(sizeof(int) * n);
+	for(int i = 0; i < n; i++) result[*returnSize][i] = pre[i];
+	(*returnSize)++;
 
 	return result;
 }
 
+int** dfs(int **result, int *pre, int pn, int* candidates, int candidatesSize, int target, int** columnSizes, int* returnSize) {
+
+	int *newPre;
+
+	for(int i = 0; i < candidatesSize; i++) {
+		for(int j = 1; candidates[i] * j <= target; j++) {
+
+			int leftSize = candidatesSize - i - 1;
+
+			if(leftSize < 0) continue;
+
+			newPre = malloc((pn + j) * sizeof(int));
+			for(int k = 0; k < pn; k++) newPre[k] = pre[k];
+			for(int k = 0; k < j; k++) newPre[k + pn] = candidates[i];
+
+			if(target - candidates[i] * j > 0)
+				result = dfs(result, newPre, pn + j, candidates + i + 1, leftSize, target - candidates[i] * j, columnSizes, returnSize);
+			else
+				result = record(result, columnSizes, returnSize, newPre, pn + j);
+			//free(newPre);
+		}
+	}
+	return result;
+}
+
 int** combinationSum(int* candidates, int candidatesSize, int target, int** columnSizes, int* returnSize) {
+
+	int **result = NULL;
 
 	*columnSizes = NULL;	
 	*returnSize = 0;
@@ -58,7 +58,7 @@ int** combinationSum(int* candidates, int candidatesSize, int target, int** colu
 
 	bubbleSort(candidates, candidatesSize);
 
-	return combinationSum2(candidates, candidatesSize, target, columnSizes, returnSize);
+	return dfs(result, NULL, 0, candidates, candidatesSize, target, columnSizes, returnSize);
 }
 
 int main() {
@@ -71,11 +71,14 @@ int main() {
 	int i, j;
 
 	result = combinationSum(candidates, candidatesSize, target, &columnSizes, &returnSize);
+
 	for (i = 0; i < returnSize; i++) {
+		int *tmp = result[i];
 		for (j = 0; j < columnSizes[i]; j++) {
-			printf("%d ", result[i][j]);
+			printf("%d ", tmp[j]);
 		}
 		printf("\n");
 	}
 	return 0;
+
 }
