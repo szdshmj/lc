d3260ac6ac52dd572b8c1649273b442381ecbf10
hmj6639 <hmj6639@126.com>
2018-02-28 16:31:47 +0800

    n43

---
+++ C/0043_multiply_strings/main.c
@@ -0,0 +1,16 @@
+#include "../inc.h"
+
+char* multiply(char* num1, char* num2) {
+
+	return NULL;
+}
+
+int main()
+{
+	char *s1="12";	
+	char *s2="22";
+
+	printf("%s*%s = %s\n", s1, s2, multiply(s1, s2));
+
+	return 0;
+}
