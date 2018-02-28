9fc8badc09bd7df4ff02eae91023ffe81eb5125c
hmj6639 <hmj6639@126.com>
2018-02-28 14:34:50 +0800

    42

--- C/0042_trap_rain_water/main.c
+++ C/0042_trap_rain_water/main.c
@@ -2,7 +2,7 @@
 
 void dump(int *height, int heightSize)
 {
-	printf("t %d\n", heightSize);
+//	printf("t %d\n", heightSize);
 	for(int i = 0; i < heightSize; i++)
 		printf("%d ", height[i]);
 	printf("\n");
@@ -10,11 +10,15 @@ void dump(int *height, int heightSize)
 
 int newRight(int s, int *height, int heightSize)
 {
-	int mark = -1;
+	int mark = -1, h = s;
 
+	//printf("compare %d\n", s);
 	for(int i = 0; i < heightSize; i++) {
-		if(height[i] >= s)
+		if(height[i] >= h) {
 			mark = i;		
+			h = height[i];
+	//		printf("find %d at %d\n", height[i], mark);
+		}
 	}
 
 	//printf("can forward %d\n", mark + 1);
@@ -25,7 +29,8 @@ int findABowl(int *height, int heightSize, int *result)
 {
 	int left = 0, buttom, right = - 1, pos, skip = 0;
 
-	while(left + 1 < heightSize && height[left + 1] > height[left]) left++;
+	dump(height, heightSize);
+	while(left + 1 < heightSize && height[left + 1] >= height[left]) left++;
 
 	pos = left + 1;
 	buttom = pos;
@@ -43,7 +48,7 @@ int findABowl(int *height, int heightSize, int *result)
 	}
 
 	while(right + 1 < heightSize && height[right + 1] > height[right])	right++;
-//	printf("left %d, right %d, \n", left, right);
+	//printf("left %d, right %d, \n", left, right);
 	
 	if(right + 1 < heightSize && height[right] < height[left])
 		skip = newRight(height[right], height + right + 1, heightSize - right - 1);
@@ -57,7 +62,7 @@ int findABowl(int *height, int heightSize, int *result)
 		for(int i = left + 1; i < right; i++) {
 			if(minH > height[i]) {
 				*result += (minH - height[i]);
-//				printf("\t\t\tresult %d\n", *result);
+				//printf("\t\t\tresult %d\n", *result);
 			}
 		}
 		return right;
@@ -72,7 +77,6 @@ int trap(int* height, int heightSize) {
 
 	while(l < heightSize) {
 
-		//printf("new l %d.................\n", l);
 		if((width = findABowl(height + l, heightSize - l, &result)) > 0)
 			l += width;
 		else
@@ -84,14 +88,32 @@ int trap(int* height, int heightSize) {
 
 int main()
 {
-	//int ret, a[] = {0,1,0,2,1,0,1,3,2,1,2,1};
-	//int ret, a[] = {4, 2, 3};
-	//int ret, a[] = {5, 4, 1, 2};
-	//int ret, a[] = {5,2,1,2,1,5};
-	int ret, a[] = {5,5,1,7,1,1,5,2,7,6};
+	int ret; 
+	int a[] = {0,1,0,2,1,0,1,3,2,1,2,1};
+	int b[] = {4, 2, 3};
+	int c[] = {5, 4, 1, 2};
+	int d[] = {5,2,1,2,1,5};
+	int e[] = {5,5,1,7,1,1,5,2,7,6};
+	int f[] = {6,4,2,0,3,2,0,3,1,4,5,3,2,7,5,3,0,1,2,1,3,4,6,8,1,3};
 
-	ret = trap(a, sizeof(a) / sizeof(a[0]));
-	printf("%d\n", ret);
+	int *p[] = {a, b ,c ,d, e, f}; 
+	int s[] = 	{sizeof(a) / sizeof(a[0]), 
+				sizeof(b) / sizeof(b[0]), 
+				sizeof(c) / sizeof(c[0]), 
+				sizeof(d) / sizeof(d[0]),
+				sizeof(e) / sizeof(e[0]),
+				sizeof(f) / sizeof(f[0]),
+				};
+	int expect[] = {6, 1, 1, 14, 23, 83};
 
+	for(int i = 0; i < sizeof(p) / sizeof(p[0]); i++){
+		ret = trap(p[i], s[i]);
+		if(ret != expect[i]) {
+			printf("index %d is wrong, expect %d, but %d\n", i, expect[i], ret);
+			return -1;
+		}
+	}
+
+	printf("all passd\n");
 	return 0;
 }
