c7feabb2f7e3acf88dbfc889ec5a6c48f23e629f
hmj6639 <hmj6639@126.com>
2018-02-28 17:07:02 +0800

    n43

--- C/0043_multiply_strings/main.c
+++ C/0043_multiply_strings/main.c
@@ -1,6 +1,19 @@
 #include "../inc.h"
 
+char* simply(char *num1, char s)
+{
+	printf("%s x %c\n", num1, s);
+}
+
 char* multiply(char* num1, char* num2) {
+
+	if(num1 == NULL || num2 == NULL)
+		return NULL;
+	
+	for(int i = 0 ; i < strlen(num2); i++) {
+		
+		simply(num1, num2[i]);
+	}
 
 	return NULL;
 }
@@ -8,7 +21,7 @@ char* multiply(char* num1, char* num2) {
 int main()
 {
 	char *s1="12";	
-	char *s2="22";
+	char *s2="34";
 
 	printf("%s*%s = %s\n", s1, s2, multiply(s1, s2));
 
