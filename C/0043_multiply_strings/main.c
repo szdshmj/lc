0ad5f01b9660de7f575e77f672279a2102f2b2f4
hmj6639 <hmj6639@126.com>
2018-03-01 23:49:14 +0800

    43

--- C/0043_multiply_strings/main.c
+++ C/0043_multiply_strings/main.c
@@ -1,56 +1,41 @@
 #include "../inc.h"
 
-void newAdd(int b, int *n, char *n1, int t1, int a)
-{
-	int i, shift = 0;
-
-	for(i = 0; i < t1; i++) {
-		n[b + i] += (a * (n1[t1 - 1 - i] - '0') + shift);
-		if(n[b + i] >= 10) {
-			shift = n[b + i] / 10;
-			n[b + i] = n[b + i] % 10;
-		}
-		else if(shift != 0)
-			shift = 0;
-	}
-	if(shift)
-		n[b + i] = shift;
-}
-
 char* multiply(char* num1, char* num2) {
 
 	char *result = NULL;
-	int a, t1, t2, i, j, shift = 0, *n;
-
-	if(num1 == NULL || num2 == NULL) return result;
+	int a, t1, t2, i, j, shift = 0;
 
 	t1 = strlen(num1);
 	t2 = strlen(num2);
-	n = calloc(sizeof(int), t1 + t2);
+	result = calloc(1, t1 + t2 + 1);
 
 	for(i = 0; i < t2; i++) {
 
 		a = num2[t2 - i - 1] - '0';
 		shift = 0;
 		for(j = 0; j < t1; j++) {
-			n[i + j] += (a * (num1[t1 - 1 - j] - '0') + shift);
-			if(n[i + j] >= 10) {
-				shift = n[i + j] / 10;
-				n[i + j] = n[i + j] % 10;
+			result[i + j] += (a * (num1[t1 - 1 - j] - '0') + shift);
+			if(result[i + j] >= 10) {
+				shift = result[i + j] / 10;
+				result[i + j] %= 10;
 			}
 			else if(shift != 0)
 				shift = 0;
 		}
 		if(shift)
-			n[i + j] = shift;
+			result[i + j] = shift;
 	}
 
-	for(i = t1 + t2 - 1; i > 0 && n[i] == 0; i--);
-	result = malloc(i + 2); 
-	for(int j = i; j > 0; j--)result[i - j] = n[j] + '0';
-	result[i] = '0' + n[0];
-	result[i + 1] = '\0';
-	free(n);
+	for(j = t1 + t2 - 1, i = 0; j > 0 && result[j] == 0; j--);
+	result[j + 1] = '\0';
+
+	while(i < j) {
+		char t = result[i] + '0';
+		result[i] = result[j] + '0';
+		result[j] = t;
+		i++;j--;
+	}
+	if(i == j)result[i] += '0';
 
 	return result;
 }
@@ -67,8 +52,9 @@ int main()
 	char *s8[] = {"123456789", "9"};
 	char *s9[] = {"123456789", "987654321"};
 	char *s10[] = {"237", "284"};
+	char *s11[] = {"156955557690951899023516531972207095742403628909031", "8406815759751608344580781082398348"};
 
-	printf("%s*%s = %s\n", s1[0], s1[1], multiply(s1[0], s1[1]));
+	/*printf("%s*%s = %s\n", s1[0], s1[1], multiply(s1[0], s1[1]));
 	printf("%s*%s = %s\n", s2[0], s2[1], multiply(s2[0], s2[1]));
 	printf("%s*%s = %s\n", s3[0], s3[1], multiply(s3[0], s3[1]));
 	printf("%s*%s = %s\n", s4[0], s4[1], multiply(s4[0], s4[1]));
@@ -77,7 +63,8 @@ int main()
 	printf("%s*%s = %s\n", s7[0], s7[1], multiply(s7[0], s7[1]));
 	printf("%s*%s = %s\n", s8[0], s8[1], multiply(s8[0], s8[1]));
 	printf("%s*%s = %s\n", s9[0], s9[1], multiply(s9[0], s9[1]));
-	printf("%s*%s = %s\n", s10[0], s10[1], multiply(s10[0], s10[1]));
+	printf("%s*%s = %s\n", s10[0], s10[1], multiply(s10[0], s10[1]));*/
+	printf("%s*%s = %s\n", s11[0], s11[1], multiply(s11[0], s11[1]));
 
 	return 0;
 }
