2555821eaf18b26385f2153aef65602a8b5b1ead
hmj6639 <hmj6639@126.com>
2018-03-01 22:54:58 +0800

    43

--- C/0043_multiply_strings/main.c
+++ C/0043_multiply_strings/main.c
@@ -1,42 +1,11 @@
 #include "../inc.h"
 
-int simply(int *tmp, char *n1, int t1, int n2)
+void newAdd(int b, int *n, char *n1, int t1, int a)
 {
-	int i, m, shift = 0;
+	int i, shift = 0;
 
 	for(i = 0; i < t1; i++) {
-
-		m = (n1[t1 - 1 - i] - '0') * n2 + shift;
-
-		if(m >= 10) {
-			shift = m / 10;
-			m = m % 10;
-		}
-		else if(shift != 0)
-			shift = 0;
-
-		tmp[i] = m;
-	}
-
-	if(shift > 0)
-		tmp[i++] = shift;
-
-#if 0
-	for(int j = 0; j < i; j++)
-		printf("%d", tmp[j]);
-	printf("\n");
-#endif
-	return i;
-}
-
-void add(int b, int *n, int *tmp, int tlen)
-{
-	int shift = 0, i;
-
-	for(i = 0; i < tlen; i++) {
-		
-		n[b + i] += (tmp[i] + shift);
-
+		n[b + i] += (a * (n1[t1 - 1 - i] - '0') + shift);
 		if(n[b + i] >= 10) {
 			shift = n[b + i] / 10;
 			n[b + i] = n[b + i] % 10;
@@ -44,36 +13,45 @@ void add(int b, int *n, int *tmp, int tlen)
 		else if(shift != 0)
 			shift = 0;
 	}
-
 	if(shift)
 		n[b + i] = shift;
-
-	for(int j = 0; j < b + i; j++)
-		printf("%d", n[j]);
-	
-	printf("\n");
 }
 
 char* multiply(char* num1, char* num2) {
-	
-	char *result = NULL;
-	int t1, t2;
-	int *n, *tmp;
 
-	if(num1 == NULL || num2 == NULL)
-		return result;
-	
+	char *result = NULL;
+	int a, t1, t2, i, j, shift = 0, *n;
+
+	if(num1 == NULL || num2 == NULL) return result;
+
 	t1 = strlen(num1);
 	t2 = strlen(num2);
 	n = calloc(sizeof(int), t1 + t2);
-	tmp = calloc(sizeof(int) ,t1 + 1);
 
-	for(int i = 0; i < t2; i++) {
-	
-		simply(tmp, num1, t1, num2[t2 - i - 1] - '0');
-		add(i, n, tmp, t1 + 1);
+	for(i = 0; i < t2; i++) {
+
+		a = num2[t2 - i - 1] - '0';
+		shift = 0;
+		for(j = 0; j < t1; j++) {
+			n[i + j] += (a * (num1[t1 - 1 - j] - '0') + shift);
+			if(n[i + j] >= 10) {
+				shift = n[i + j] / 10;
+				n[i + j] = n[i + j] % 10;
+			}
+			else if(shift != 0)
+				shift = 0;
+		}
+		if(shift)
+			n[i + j] = shift;
 	}
-		
+
+	for(i = t1 + t2 - 1; i > 0 && n[i] == 0; i--);
+	result = malloc(i + 2); 
+	for(int j = i; j > 0; j--)result[i - j] = n[j] + '0';
+	result[i] = '0' + n[0];
+	result[i + 1] = '\0';
+	free(n);
+
 	return result;
 }
 
@@ -88,6 +66,7 @@ int main()
 	char *s7[] = {"9999", "9"};
 	char *s8[] = {"123456789", "9"};
 	char *s9[] = {"123456789", "987654321"};
+	char *s10[] = {"237", "284"};
 
 	printf("%s*%s = %s\n", s1[0], s1[1], multiply(s1[0], s1[1]));
 	printf("%s*%s = %s\n", s2[0], s2[1], multiply(s2[0], s2[1]));
@@ -98,6 +77,7 @@ int main()
 	printf("%s*%s = %s\n", s7[0], s7[1], multiply(s7[0], s7[1]));
 	printf("%s*%s = %s\n", s8[0], s8[1], multiply(s8[0], s8[1]));
 	printf("%s*%s = %s\n", s9[0], s9[1], multiply(s9[0], s9[1]));
+	printf("%s*%s = %s\n", s10[0], s10[1], multiply(s10[0], s10[1]));
 
 	return 0;
 }
