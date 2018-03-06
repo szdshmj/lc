aef210248bd94eec15f0f5b603550563ccfb75f8
hmj6639 <hmj6639@126.com>
2018-03-06 10:46:43 +0800

    48

--- C/0048_rotate_matrix/main.c
+++ C/0048_rotate_matrix/main.c
@@ -19,7 +19,7 @@ void rotate(int** matrix, int matrixRowSize, int *matrixColSizes) {
 			
 			int tmp = matrix[i][j];	
 			matrix[i][j] = matrix[n - 1 - j][i];
-			matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1- j];
+			matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
 			matrix[n - 1 - i][n - 1 -j] = matrix[j][n - 1 - i];
 			matrix[j][n - 1 - i] = tmp;
 		}
