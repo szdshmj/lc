3c144aaa0d0f80e7d6b4323a409cfc4e24832873
hmj6639 <hmj6639@126.com>
2018-03-08 07:10:57 +0800

    n45

--- C/0045_jump_game/main.c
+++ C/0045_jump_game/main.c
@@ -16,9 +16,40 @@ You can assume that you can always reach the last index.
 */
 int jump(int *nums, int numsSize)
 {
+	int *help = malloc(sizeof(int) * numsSize);
+	int *step = malloc(sizeof(int) * numsSize);
+
+	for(int i = 0; i < numsSize; i++) help[i] = 0;
+	for(int i = 0; i < numsSize; i++) step[i] = 0;
+
+	for(int i = numsSize - 2; i >= 0; i--) {
+
+		if((nums[i] + i) >= numsSize -1) {
+			help[i] = 1;
+			step
+		}
+		else {
+			
+			int next, min = INT_MAX, jj;
+			for(int j = 1; j <= nums[i]; j++) {
+				
+				next = i + j;
+				if(help[next] < min) {
+					min = help[next];
+					jj = j;
+				}
+			}
+			help[i] = min + 1;
+		}
+	}
+	for(int i = 0; i < numsSize; i++) printf("%d ", help[i]);
+	printf("\n");
 }
 
 int main()
 {
+	int a[] = {2, 3, 1, 1, 4};
+
+	jump(a, sizeof(a) / sizeof(a[0]));
 	return 0;
 }
