2e011d5c327f9f0fd5e09d1fd52d56feedf2932a
hmj6639 <hmj6639@126.com>
2018-03-07 11:54:50 +0800

    50

--- C/0050_pow/main.c
+++ C/0050_pow/main.c
@@ -1,9 +1,20 @@
 #include "../inc.h"
+double fast_pow(double x, int n)
+{
+    if (n == 0) { return 1.0; }
+    if (n == 1) { return x; }
+    double t = fast_pow(x, n / 2);
+    return n & 1 ? t * t * x : t * t;
+}
 
-double myPow(double x, int n) {
-
+double myPow(double x, int n)
+{
+	double t = fast_pow(x, n);
+	return n < 0 ? 1 / t : t;
 }
 
 int main()
 {
+	printf("%f\n", myPow(1.00000, -2147483648));
+	return 0;
 }
