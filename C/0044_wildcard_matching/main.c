12441fb28d7bcf0fa9288ad830e3848515af8e2a
hmj6639 <hmj6639@126.com>
2018-03-08 22:19:51 +0800

    44

---
+++ C/0044_wildcard_matching/main.c
@@ -0,0 +1,28 @@
+#include "../inc.h"
+
+bool isMatch(char* s, char* p) {
+
+	char *ss = NULL, *pp = NULL;
+
+	while(s[0] != '\0') {
+
+		if(s[0] == p[0] || p[0] == '?')
+			s++, p++;
+		else if(p[0] == '*')
+			ss = s, pp = p++;
+		else if(pp != NULL)
+			s = ++ss, p = pp + 1;
+		else
+			return false;
+	}
+
+	while(p[0] == '*')
+		p++;
+
+	return p[0] == '\0';
+}
+
+int main()
+{
+
+}
