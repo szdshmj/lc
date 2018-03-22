e2ad9a0fc433a987193818c64e84ffc08a8d2170
hmj6639 <hmj6639@126.com>
2018-03-22 17:40:56 +0800

    n78

--- C/0078_subsets/main.c
+++ C/0078_subsets/main.c
@@ -3,7 +3,8 @@
 
 int** subset(int n, int* nums, int numsSize, int** columnSizes, int* returnSize) {
 
-	if(n == 0)
+	if(n == 0) {
+	}
 }
 
 int** subsets(int* nums, int numsSize, int** columnSizes, int* returnSize) {
@@ -20,23 +21,24 @@ int** subsets(int* nums, int numsSize, int** columnSizes, int* returnSize) {
 
 int main(int argc, char *argv[])
 {
-	int *nums, numsSize, **ret, *columnSizes, returnSize;
+	int *nums, numsSize, **ret, *columnSizes, returnSize, i, j;
 
 	if(argc < 2) return -1;
 
 	numsSize = atoi(argv[1]);
 	nums = malloc(sizeof(int) * numsSize);
 
-	for(int i = 0; i < numsSize; i++) nums[i] = i;
+	while(i < numsSize) nums[i] = ++i;
 	
 	ret = subsets(nums, numsSize, &columnSizes, &returnSize);	
 
-	for(int i = 0; i < returnSize; i++) {
+	for(i = 0; i < returnSize; i++) {
 
-		for(int j = 0; j < columnSizes[i]; j++)
+		for(j = 0; j < columnSizes[i]; j++)
 			printf("%2d ", ret[i][j]);
 
 		printf("\n");
 	}
+
 	return 0;
 }
