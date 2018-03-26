96a9fe46ef8cf06d6262b90a6818be0a6b75431b
hmj6639 <hmj6639@126.com>
2018-03-26 14:23:03 +0800

    82

--- C/0082_delete_deplicates/main.c
+++ C/0082_delete_deplicates/main.c
@@ -1,6 +1,26 @@
 #include "../inc.h"
 #include "../node.h"
 
+struct ListNode* deleteDuplicatesWithDummy(struct ListNode* head) {
+
+	struct ListNode dummy = {0, head};
+	struct ListNode *p = head, *next = head, *prev = &dummy;
+
+	while (p != NULL) {
+
+		while (next != NULL && next->val == p->val)
+			next = next->next;
+
+		if (p->next == next)
+			prev = p;
+		else
+			prev->next = next;
+
+		p = next;
+	}
+	return dummy.next;
+}
+
 struct ListNode* deleteDuplicates(struct ListNode* head) {
 
 	struct ListNode *base, *p, *q;
@@ -43,10 +63,10 @@ struct ListNode* deleteDuplicates(struct ListNode* head) {
 
 int main()
 {
-	int a[] = {1, 1, 2};
+	int a[] = {1, 1, 1, 2, 3, 3, 3, 3, 4};
 	struct ListNode *head = makeList(a, sizeof(a) / sizeof(a[0]));
 
-	head = deleteDuplicates(head);
+	head = deleteDuplicatesWithDummy(head);
 	dump_list_node(head);
 
 	return 0;
