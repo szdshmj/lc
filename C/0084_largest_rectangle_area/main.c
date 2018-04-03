3c4126f215bbbbc18e581cb859088bff62bb5fdf
hmj6639 <hmj6639@126.com>
2018-04-03 14:14:03 +0800

    0084

---
+++ C/0084_largest_rectangle_area/main.c
@@ -0,0 +1,44 @@
+#include "../inc.h"
+
+int largestRectangleArea(int* heights, int heightsSize) { // {2,1,5,6,2,3};
+	
+	int max = 0, top = -1, bar, area, i;
+	int *stack = malloc(sizeof(int) * heightsSize);
+
+	for(i = 0; i < heightsSize;) {
+		
+		if(top == -1 || heights[i] > heights[stack[top]]) {
+			stack[++top] = i++;
+		}
+		else {
+
+			bar = stack[top--];
+			area = heights[bar] * (top == -1 ? i : i - stack[top] - 1);
+
+			if(area > max)
+				max = area;
+		}
+	}
+
+	while(top != -1) {
+
+		bar = stack[top--];
+		area = heights[bar] * (top == -1 ? i : i - stack[top] - 1);
+
+		if(area > max)
+			max = area;
+	}
+
+	return max;
+}
+
+int main()
+{
+	int a[] = {2,1,5,6,2,3};
+	//int a[] = {1,2,3,4,5,6};
+	int ret = largestRectangleArea(a, sizeof(a) / sizeof(a[0]));
+
+	printf("%d\n", ret);		
+
+	return 0;
+}
